5d81bcde13202bd2657e6e674f5277b44a99e580
akruti-addweb <akruti@addwebsolution.in>
2023-03-27 17:44:45 +0530

    Change button component text

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -27,18 +27,18 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/html/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'View All',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'View All',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'View All',
 };
 
 export const Small = Template.bind({});
